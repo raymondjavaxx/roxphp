7943e253fee0ef9044dfaa0d2739ca3acf73bff7
Ramon Torres <raymondjavaxx@gmail.com>
2008-07-19 22:21:49 +0000

    added i18n and L10n support

---
+++ rox/language.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * Language
+ *
+ * This Software is released under the MIT License.
+ * See license.txt for more details.
+ *
+ * @package	rox
+ * @author Ramon Torres
+ * @copyright Copyright (c) 2008 Ramon Torres
+ * @license http://roxphp.com/static/license.html
+ * @link http://roxphp.com 
+ * @access public
+ */
+class Language {
+
+	/**
+	 * Locale instance
+	 * 
+	 * @var Locale
+	 */
+	protected $locale;
+
+	/**
+	 * Loaded translation tables
+	 * 
+	 * @var array
+	 */
+	protected $tables = array();
+
+	/**
+	 * Language::init()
+	 * 
+	 * @param Locale $locale
+	 */
+	public function init($locale = null) {
+		if (is_null($locale)) {
+			$locale = new Locale;
+		} else if (!($locale instanceof Locale)) {
+			throw new Exception('Param must be instance of Locale');
+		}
+
+		$this->locale = $locale;
+	}
+
+	/**
+	 * Language::getInstance()
+	 *
+	 * @return Language
+	 */
+	public static function getInstance() {
+		static $instance;
+		if (!is_object($instance)) {
+			$instance = new Language;
+		}
+
+		return $instance;
+	}
+
+	/**
+	 * Language::translateText()
+	 * 
+	 * @param mixed $text
+	 * @param string $table
+	 * @return string
+	 */
+	public function translateText($text, $table = 'messages') {
+		if (!isset($this->tables[$table][$text])) {
+			return $text;
+		}
+
+		return $this->tables[$table][$text];
+	}
+
+	/**
+	 * Language::_()
+	 * 
+	 * @param mixed $text
+	 * @param string $table
+	 * @return string
+	 */
+	public static function _($text, $table = 'messages') {
+		return Language::getInstance()->translateText($text, $table);
+	}
+
+	/**
+	 * Language::loadTable()
+	 *
+	 * @return void
+	 */
+	public function loadTable($name = 'messages') {
+		$fp = @fopen(APP . 'locale' . DS . $this->locale->getTag() . DS . $name . '.csv', 'r');
+		if ($fp === false) {
+			throw new Exception('Can`t open locale file');
+		}
+
+		while (($data = fgetcsv($fp)) !== false) {
+			$this->tables[$name][$data[0]] = $data[1];
+		}
+
+		fclose($fp);
+	}
+}
---
+++ rox/locale.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Locale
+ *
+ * This Software is released under the MIT License.
+ * See license.txt for more details.
+ *
+ * @package	rox
+ * @author Ramon Torres
+ * @copyright Copyright (c) 2008 Ramon Torres
+ * @license http://roxphp.com/static/license.html
+ * @link http://roxphp.com 
+ * @access public
+ */
+class Locale {
+
+	/**
+	 * Locale tag
+	 * 
+	 * @var string
+	 */
+	protected $tag = 'en_US';
+
+	/**
+	 * Returns current locale tag
+	 * 
+	 * @return string
+	 */
+	public function getTag() {
+		return $this->tag;
+	}
+
+	/**
+	 * Sets current locale tag
+	 * 
+	 * @param string $tag
+	 */
+	public function setTag($tag) {
+		$this->tag = $tag;
+	}
+
+	/**
+	 * Autodetect the locale based on browser preferences
+	 *
+	 * @param array $available
+	 * @param bool $fallback
+	 * @return boolean
+	 */
+	public function autoDetect($available, $fallback = true) {
+		$locale = $this->getBrowserLocale();
+		if ($locale === false) {
+			return false;
+		}
+
+		if (in_array($locale, $available)) {
+			$this->currentLanguage = $locale;
+			return true;
+		} 
+
+		if ($fallback) {
+			$subTags = explode('_', $locale);
+			foreach ($available as $a) {
+				if (strpos($a, $subTags[0]) === 0) {
+					$this->tag = $a;
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Gets locale tag from browser
+	 * 
+	 * @return string|false
+	 */
+	protected function getBrowserLocale() {
+		if (!isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+			return false;
+		}
+
+		$parts = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+		if (!isset($parts[0])) {
+			return false;
+		}
+
+		if (($subTags = explode('-', $parts[0])) === false) {
+			return false;
+		}
+
+		if (preg_match('/^[a-z]{2,3}/', $subTags[0]) !== 1) {
+			return false;
+		}
+
+		$locale = strtolower($subTags[0]);
+
+		if (isset($subTags[1]) && preg_match('/^[A-Z]{2}$/', $subTags[1]) === 1) {
+			$locale .= '_' . strtoupper($subTags[1]);
+		} else if (isset($subTags[2]) && preg_match('/^[A-Z]{2}$/', $subTags[2]) === 1) {
+			$locale .= '_' . strtoupper($subTags[2]);
+		}
+
+		return $locale;
+	}
+}
