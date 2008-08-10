1283ce9baf235143a957ffab3a65e53bacfa31b9
Ramon Torres <raymondjavaxx@gmail.com>
2008-08-10 03:18:47 +0000

    refactored Registry class and removed singleton

--- rox/registry.php
+++ rox/registry.php
@@ -11,7 +11,7 @@
  * @author Ramon Torres
  * @copyright Copyright (c) 2008 Ramon Torres (http://roxphp.com)
  * @license http://roxphp.com/static/license.html
- * @version $Id:$
+ * @version $Id$
  */
 
 /**
@@ -21,55 +21,75 @@
  * @copyright Copyright (c) 2008 Ramon Torres
  * @license http://roxphp.com/static/license.html
  */
-class Registry extends Object {
+class Registry {
 
-	private $objects = array();
+	/**
+	 * Object instances
+	 *
+	 * @var array
+	 */
+	private static $_objects = array();
 
-	private static $_instance;
-
-  /**
-   * Registry::getInstance()
-   *
-   * @return
-   */
-	public static function &getInstance() {
-		if (empty(self::$_instance)) {
-			self::$_instance = new Registry();
-		}
-
-		return self::$_instance;
-	}
-
-  /**
-   * Registry::addObject()
-   *
-   * @param mixed $name
-   * @param mixed $instance
-   * @return
-   */
-	public static function addObject($name, &$instance) {
-		$_self = Registry::getInstance();
-		if (!isset($_self->objects[$name])) {
-			$_self->objects[$name] = &$instance;
+	/**
+	 * Adds an object to the registry
+	 *
+	 * @param string $name
+	 * @param object $instance
+	 * @return boolean
+	 */
+	public static function addObject($name, $instance) {
+		if (!self::contains($name)) {
+			self::$_objects[$name] = $instance;
 			return true;
 		}
 
 		return false;
 	}
 
-  /**
-   * Registry::getObject()
-   *
-   * @param mixed $name
-   * @return
-   */
-	public static function &getObject($name) {
-		$_self = Registry::getInstance();
-		if (isset($_self->objects[$name])) {
-			return $_self->objects[$name];
+	/**
+	 * Retrieves an object from the registry 
+	 *
+	 * @param string $name
+	 * @return object|false
+	 */
+	public static function getObject($name) {
+		if (self::contains($name)) {
+			return self::$_objects[$name];
 		}
 
-		$result = false;
-		return $result;
+		return false;
+	}
+
+	/**
+	 * Removes an object from the registry
+	 * 
+	 * @param string $name
+	 * @return boolean
+	 */
+	public function removeObject($name) {
+		if (self::contains($name)) {
+			unset(self::$_objects[$name]);
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Checks if the registry contains a given object 
+	 * 
+	 * @param string $name
+	 * @return boolean
+	 */
+	public static function contains($name) {
+		return array_key_exists($name, self::$_objects);
+	}
+
+	/**
+	 * Returns the number of objects in the registry
+	 * 
+	 * @return integer
+	 */
+	public static function size() {
+		return count(self::$_objects);
 	}
 }
