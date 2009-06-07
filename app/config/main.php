900f1e545b47fbbfa3f4cbb0fc90776c4dc5e0f7
Ramon Torres <raymondjavaxx@gmail.com>
2009-06-07 04:33:56 +0000

    timezone for date

--- app/config/main.php
+++ app/config/main.php
@@ -7,12 +7,12 @@
  *
  * @package	rox
  * @author Ramon Torres
- * @copyright Copyright (c) 2008 Ramon Torres
+ * @copyright Copyright (c) 2008 - 2009 Ramon Torres
  * @license http://roxphp.com/static/license.html
  * @link http://roxphp.com 
- * @access public
  */
+
+date_default_timezone_set('America/New_York');
 
 define('ROX_DEBUG', true);
 define('ROX_DEFAULT_ROUTE', 'pages/home');
-?>
