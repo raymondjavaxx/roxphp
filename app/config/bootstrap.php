dcae3f8eccd5f0f8c9ddb9b3f15757dece31bf47
rtorres <raymondjavaxx@gmail.com>
2010-06-26 22:36:22 -0400

    add vendors folder to include path

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -39,6 +39,7 @@ set_include_path(implode(PATH_SEPARATOR, array(
 	ROX_APP_PATH . '/controllers',
 	ROX_APP_PATH . '/helpers',
 	ROX_APP_PATH . '/mailers',
+	ROX_APP_PATH . '/vendors',
 	ROX_FRAMEWORK_PATH,
 	// uncoment the line below preserve default include paths
 	// get_include_path()
