aa94c83e7d05e9df200251e61fcebbdd60f354a9
Ramon <raymondjavaxx@gmail.com>
2010-08-06 12:48:43 -0400

    fixed: exists() returning false when more than one record match the query

--- rox/ActiveRecord.php
+++ rox/ActiveRecord.php
@@ -526,7 +526,7 @@ abstract class Rox_ActiveRecord {
 		$sql.= ' LIMIT 1';
 
 		$result = $this->datasource()->query($sql);
-		return $result[0]['count'] == 1;
+		return $result[0]['count'] > 0;
 	}
 
 	/**
