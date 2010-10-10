c1ca617b164dc38d5b349e5b2455b211df7cd4f2
rtorres <raymondjavaxx@gmail.com>
2010-10-10 02:04:57 -0400

    skip test when SMTP server is not present

--- rox/test/Rox/Mailer/Adapter/SmtpTest.php
+++ rox/test/Rox/Mailer/Adapter/SmtpTest.php
@@ -4,6 +4,14 @@ require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/helper.php';
 
 class Rox_Mailer_Adapter_SmtpTest extends PHPUnit_Framework_TestCase {
 
+	protected function setUp() {
+		if ($f = @fsockopen('127.0.0.1', 25, $errno, $errstr, 1)) {
+			fclose($f);
+		} else {
+			$this->markTestSkipped('No SMTP server running on localhost.');
+		}
+	}
+
 	public function testSend() {
 		$message = new Rox_Mailer_Message(array(
 			'from' => 'ramon@example.org',
