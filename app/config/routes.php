e950676287052d9dbe08f18f34b5d30560c1e43c
rtorres <raymondjavaxx@gmail.com>
2011-02-28 23:30:26 -0500

    updated copyright notice

--- app/config/routes.php
+++ app/config/routes.php
@@ -2,12 +2,12 @@
 /**
  * RoxPHP
  *
- * Copyright (C) 2008 - 2010 Ramon Torres
+ * Copyright (C) 2008 - 2011 Ramon Torres
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright Copyright (c) 2008 - 2010 Ramon Torres
+ * @copyright Copyright (c) 2008 - 2011 Ramon Torres
  * @package App
  * @license The MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
