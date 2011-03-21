701856fe8ecf37f2da39c5aa96996b8abda73ee9
Chris Roush <roush.chris@gmail.com>
2011-03-21 13:31:04 -0400

    Added Generate Objects

---
+++ rox/Console/Command/Views.php
@@ -0,0 +1,14 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Rox_Console_Command_Views extends Rox_Console_Command_Gen{
+
+	function construct($name){
+		
+	}
+}
+
+?>
