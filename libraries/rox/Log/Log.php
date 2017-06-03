b6244891838cc57d6b306bde9ec2e5d0d2c6b4eb
Ramon Torres <raymondjavaxx@gmail.com>
2017-06-03 02:00:09 -0400

    Added missing log levels

--- libraries/rox/Log/Log.php
+++ libraries/rox/Log/Log.php
@@ -40,15 +40,45 @@ class Log
 		return static::$logger;
 	}
 
-	public static function warning($message, array $context = [])
-	{
-		static::logger()->warning($message, $context);
-	}
+    public static function emergency($message, array $context)
+    {
+		static::logger()->emergency($message, $context);
+    }
 
-	public static function error($message, array $context = [])
-	{
+    public static function alert($message, array $context)
+    {
+		static::logger()->alert($message, $context);
+    }
+
+    public static function critical($message, array $context)
+    {
+		static::logger()->critical($message, $context);
+    }
+
+    public static function error($message, array $context)
+    {
 		static::logger()->error($message, $context);
-	}
+    }
+
+    public static function warning($message, array $context)
+    {
+		static::logger()->warning($message, $context);
+    }
+
+    public static function notice($message, array $context)
+    {
+		static::logger()->notice($message, $context);
+    }
+
+    public static function info($message, array $context)
+    {
+		static::logger()->info($message, $context);
+    }
+
+    public static function debug($message, array $context)
+    {
+		static::logger()->debug($message, $context);
+    }
 
 	public static function write($type, $message, array $context = [])
 	{
